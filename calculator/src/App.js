9e0a8e3ea7ae11b47c3ca2d065c7318f599867fe
Jubayer Ahmed Riad <jubayer999@gmail.com>
2022-04-21 00:06:28 +0200

    I have made the front end of the calculator and commiting

---
+++ calculator/src/App.js
@@ -0,0 +1,52 @@
+import "./styles.css"
+import { useReducer } from "react";
+import { type } from "@testing-library/user-event/dist/type";
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CLEAR: "clear-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  REMOVE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate"
+}
+
+function reducer(state,{operationType,payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return state
+  }
+
+
+}
+
+function App() {
+  const [{currentOperand,previousOperand,operation},dispatch] = useReducer(reducer,{})
+
+  return (
+    <div className="calculator-grid" >
+      <div className="output">
+        <div className="previous-operand">{previousOperand}{operation}</div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+}
+
+export default App;
